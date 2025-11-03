504a6d000e73bc39b82b958b7f04a4453bcb1757
Darren Ng <>
2025-11-03 18:16:24 +0800

    html.sh vowel

--- html.sh
+++ html.sh
@@ -2,31 +2,34 @@
 
 H=/tmp/x.html
 
-A=(00 27 D5 28 7E 2A 2C 86 2E 2F 31 32 98 33 34 3A 41 42 43 AF AD 44 45 46 BE 48 C7 C6 C8 CB D0 49 4A)
-[[ "33" == "${#A[@]}" ]]
+#V=(1 2 25 26 27 28 30 31)
+#C=(3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 29 32)
+A=(00 27 D5 28 7E 2A 2C 86 2E 2F 31 32 98 33 34 3A 41 42 43 AF AD 44 45 46 BE 48 C7 C6 C8 CB D0 49)
+A+=(26 40)
+[[ "34" == "${#A[@]}" ]]
 
-V=(1 2 25 26 27 28 30 31)
-C=(3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 29 32)
+V4=(1 2 25 26 27 28)
+V8=(30 31)
 
-#V4=(1 2 25 26 27 28)
-#V6=(30 31)
-#V=
-#
-#C3=(                                                        24
-#C2=(            9 10 11 12                                     29
-#C4=(3 4 5 6 7 8            13 14 15 16 17 18 19 20 21 22 23       32
+C2=(             9 10 11 12                                     29    )
+C3=(                                                         24       )
+C4=( 3 4 5 6 7 8            13 14 15 16 17 18 19 20 21 22 23       32 )
 
-g() {
+f() {
 	for i in "$@"; do
 		j="${A["$i"]}"
 		echo -ne "\u06$j"
 	done
 }
 
+# grey
+g() {
+	echo -n "<span style=color:#EEEEEE>$(f "$@")</span>"
+}
+
 echo
 
-{
-cat <<EOF
+{ cat <<EOF
 <html>
 <head>
 <meta charset="UTF-8">
@@ -35,30 +38,79 @@ cat <<EOF
 <pre style='font-family:"Noto Serif Old Uyghur";'>
 EOF
 printf "\n"
-for i in "${V[@]}"; do
-	g "$i"
-	printf " "
-done
-printf "\n\n"
-for i in "${V[@]}"; do
-	printf "$i = "
-	g 32 "$i"
-	printf " "
-	g "$i"
-	printf " "
-	g 21 "$i"
-	printf " "
-	g 21 32 "$i"
+
+	# V4
+	for i in "${V4[@]}"; do
+		printf "$i - "
+		f 32 "$i"
+		printf " "
+		f "$i"
+		printf " "
+		g 21
+		f "$i"
+		printf " "
+		g 21
+		f 32 "$i"
+		echo
+	done
+	printf "\n"
+
+	# V8
+	for i in "${V8[@]}"; do
+	#for i in 31; do
+		printf "$i - "
+		
+		f 32 $i
+		printf " "
+
+		f $i
+		printf " "
+
+		f 32 $i
+		g 1
+		printf " "
+
+		f $i
+		g 21
+		printf " "
+
+		g 21
+		f $i
+		g 1
+		printf " "
+
+		g 21
+		f 32 $i
+		g 1
+		printf " "
+
+		g 21
+		f $i
+		printf " "
+
+		g 21
+		f 32 $i
+		printf " "
+
+		echo
+	done
+	printf "\n"
+
+	# C2
+
+	# C3
+
+	# C4
+
+	# alphabet listing
+	for i in {1..32}; do
+		printf "$i - "
+		f "$i"
+		echo
+	done
 	echo
-done
-printf "\n"
-for i in "${C[@]}"; do
-	g "$i"
-	printf " "
-done
-printf "\n\n"
-echo "<""/pre></html>"
-} | tee /tmp/x.html
+
+echo "<""/pre></html>"; } | tee /tmp/x.html
 
 printf "\nw $H\n\n"
 
